43f26640cd613d552b44a4c1b880efcea195e45e
DebarunMitra <debarun96mitra@gmail.com>
2019-12-05 11:27:29 +0530

    sorting and provider added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import { SearchBox } from './components/search-box/search-box.component';
 import { Courses } from './components/course/course.component';
+import { ProviderFilterBox } from './components/provider-filter-box/provider-filter-box.component';
 import Spinner from './components/Spinner';
 
 class App extends Component {
@@ -10,7 +11,10 @@ class App extends Component {
   this.state={
     courses:[],
     searchField:'',
-    finalSearch:''
+    finalSearch:'',
+    providerSearch:'',
+    provider:'',
+    sessionDate:''
   };
 }
 componentDidMount(){
@@ -27,23 +31,37 @@ getSearch = e => {
       e.preventDefault();
       this.setState({finalSearch:this.state.searchField});
     };
-sortcoursebylength=e=>{
-        e.preventDefault();
-  console.log('sort');
+sortValueAsen=e=>{
+      e.preventDefault();
+      //  console.log('sort');
+      this.setState({courses:this.state.courses.sort((a,b)=>a.Length-b.Length)});
+      //console.log(this.state.courses);
 }
-
+sortValueDesen=e=>{
+  e.preventDefault();
+  this.setState({courses:this.state.courses.sort((a,b)=>b.Length-a.Length)});
+}
+updateProvider=e=>{
+        this.setState({providerSearch:e.target.value});
+}
+getProvider=e=>{
+    e.preventDefault();
+    this.setState({provider:this.state.providerSearch});
+}
   render(){
-    const {courses,searchField,finalSearch}=this.state;
-    // console.log(finalSearch);
+    const {courses,finalSearch,provider}=this.state;
+    let regexProvider='';
+    (provider==='')?regexProvider = new RegExp(/.*\S.*/, "gi"):regexProvider = new RegExp("\\b(?:"+provider+")\\b", "gi");
     const filteredCourses=courses.filter((course) =>
-        course['Child Subject'].toLowerCase().includes(finalSearch.toLowerCase())
+         course['Child Subject'].toLowerCase().includes(finalSearch.toLowerCase())
          && course.Length!=='' &&
          course.Length!==0 &&
          course['Child Subject']!=='' &&
          course['Next Session Date']!=='' &&
-         course.Provider!==''
+         course.Provider.match(regexProvider) &&
+         course['Universities/Institutions'].match(/.*\S.*/)
     );
-  //  filteredCourses.sort((a,b)=>a.Length-b.Length)
+
     return (
       <div className="App">
         <header className="header">
@@ -52,9 +70,22 @@ sortcoursebylength=e=>{
           <form onSubmit={this.getSearch} className="search-form">
             <SearchBox updateSearch={this.updateSearch} placeholder="Search Courses"/>
           </form>
-          <input type="button" sortcoursebylength={this.sortcoursebylength} value="Sort" />
           </div>
           {(filteredCourses.length)?(<h6 className="total-course">Course Found: {filteredCourses.length}</h6>):<Spinner />}
+          {(filteredCourses.length)?(
+            <div className="filter-sort-course">
+            <label>Course Length:</label>
+              <form onSubmit={this.sortValueAsen} className="search-form">
+                <button className="btn-sort" type="submit"><i className="fas fa-sort-numeric-down"></i></button>
+              </form>
+              <form onSubmit={this.sortValueDesen} className="search-form">
+                <button className="btn-sort" type="submit"><i className="fas fa-sort-numeric-up"></i></button>
+              </form>
+              <form onSubmit={this.getProvider} className="search-form">
+                <ProviderFilterBox updateProvider={this.updateProvider} placeholder="Search Provider"/>
+              </form>
+            </div>
+          ):false}
         </header>
             <div className="courses">
           {filteredCourses.map((course,index)=>(
